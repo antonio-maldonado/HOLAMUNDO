647dd1b16cc68037f6867735b93479b9a61f903d
Antonio Maldonado <artorio.13.45@gmail.com>
2020-06-27 19:42:52 -0500

    pwm

---
+++ build/default/production/main.o.d
@@ -0,0 +1,4 @@
+build/default/production/main.o:  \
+	C:/Users/darks/Documents/Sistemas\ de\ comunicacion/PWM.X/main.asm  \
+	C:/Users/darks/Documents/Sistemas\ de\ comunicacion/PWM.X/CLOCK.inc  \
+
